f58a2f69aeb1353b85815d9a01474aa2d5336fda
harindu-pe <harindu.pe@gmail.com>
2021-11-21 13:44:57 +0530

    update_2

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home({ posts }) {
     <div className="container mx-auto px-10 mb-8">
       <Head>
         <title>Yugen</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico/" />
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">

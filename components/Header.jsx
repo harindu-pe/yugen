51dd707190e460aa0beca61719d4346ab8b3bde1
harindu-pe <harindu.pe@gmail.com>
2021-11-24 19:35:30 +0530

    update-4

--- components/Header.jsx
+++ components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
       <div className="border-b w-full inline-block border-black-400 py-8">
         <div className="md:float-left block">
           <Link href="/">
-            <span className="transition duration-500 ease hover:bg-blue-100 inline-block rounded-full px-4 cursor-pointer font-bold text-4xl text-black">
+            <span className="transition duration-500 ease hover:bg-blue-500 inline-block rounded-full px-4 py-1 cursor-pointer font-bold text-4xl text-white">
               Yugen
             </span>
           </Link>
@@ -24,7 +24,7 @@ const Header = () => {
         <div className="hidden md:float-left md:contents">
           {categories.map((category) => (
             <Link key={category.slug} href={`/category/${category.slug}`}>
-              <span className="md:float-right mt-2 align-middle text-black ml-4 font-semibold cursor-pointer">
+              <span className="transition duration-500 ease hover:bg-blue-500 rounded-full px-2 md:float-right mt-2 align-middle text-white ml-4 font-semibold cursor-pointer">
                 {category.name}
               </span>
             </Link>
